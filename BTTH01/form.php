32931f5c0624c3a048166cfd6d6d71451d80d86a
unknown <dinhphuc25112002@gmail.com>
2023-05-08 15:22:21 +0700

    BTTH01

--- BTTH01/form.php
+++ BTTH01/form.php
@@ -1,21 +1,37 @@
-class Student {
-    public $name;
-    public $age;
-    public $major;
+<?php
 
-    public function __construct($name, $age, $major) {
-        $this->name = $name;
-        $this->age = $age;
-        $this->major = $major;
-    }
+// Đường dẫn đến file dữ liệu sinh viên
+$file_path = "students.txt";
+
+// Mở tập tin và đọc nội dung
+$file_contents = file($file_path);
+
+// Danh sách sinh viên
+$student_list = array();
+
+// Lặp qua các dòng dữ liệu và chia các trường
+foreach ($file_contents as $line) {
+  // Chia các trường bằng dấu phẩy
+  $fields = explode(",", $line);
+
+  // Tạo một mảng chứa thông tin sinh viên
+  $student = array(
+    "id" => $fields[0],
+    "name" => $fields[1],
+    "age" => $fields[2],
+    "grade" => $fields[3]
+  );
+
+  // Thêm sinh viên vào danh sách sinh viên
+  array_push($student_list, $student);
 }
 
-$students = array();
-
-$file_content = file('students.txt');
-foreach ($file_content as $line) {
-    $student_info = explode(',', $line);
-    $student = new Student(trim($student_info[0]), intval($student_info[1]), trim($student_info[2]));
-    $students[] = $student;
+// Hiển thị danh sách sinh viên
+foreach ($student_list as $student) {
+  echo "ID: " . $student["id"] . "<br>";
+  echo "Name: " . $student["name"] . "<br>";
+  echo "Age: " . $student["age"] . "<br>";
+  echo "Grade: " . $student["grade"] . "<br><br>";
 }
 
+?>
