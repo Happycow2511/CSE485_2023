1720dc007cd3b3c520f70fa6d871b2b98aa35e04
unknown <dinhphuc25112002@gmail.com>
2023-05-22 08:25:05 +0700

    BTTH02

--- BTTH02/db/db_connection.php
+++ BTTH02/db/db_connection.php
@@ -1,16 +1,14 @@
 <?php
-$servername = "localhost"; 
-$username = "root";  
-$password = "2511";  
-$database = "student_management";  
+$servername = "localhost";
+$username = "root";
+$password = "2511";
+$dbname = "student_management";
 
-// Tạo đối tượng kết nối PDO
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    // Đặt chế độ lỗi PDO để hiển thị thông báo lỗi
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+// kết nối đến cơ sở dữ liệu
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
